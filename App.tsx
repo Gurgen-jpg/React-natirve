dbf1ada4109eeda8d38d43f9b23b3c19eb4228b5
Gurgen <gsarkisdganyan@gmail.com>
2022-07-01 22:03:02 +0300

    swipe

--- App.tsx
+++ App.tsx
@@ -10,18 +10,107 @@ import {
     View,
     SafeAreaView,
     TextInput,
-    TouchableOpacity
+    TouchableOpacity, ListRenderItem, FlatList, Keyboard, TouchableWithoutFeedback
 } from 'react-native';
-import {IButton} from "./Button/Button";
-import TeslaApp from "./TeslaApp/TeslaApp";
+import Icon1 from "./icons/Icon1";
+import Icon2 from "./icons/Icon2";
+import Icon3 from "./icons/Icon3";
+import {GestureHandlerRootView} from "react-native-gesture-handler";
+import Swipeable from "react-native-gesture-handler/Swipeable";
+
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
 
 export default function App() {
     const [value, setValue] = useState('')
-    return (
-        <>
 
-            <TeslaApp/>
-           {/*<View style={styles.container}>*/}
+
+    const [tasks, setTask] = useState<TaskType[]>([
+        {id: 1, title: 'HTML', isDone: true},
+        {id: 2, title: 'REACT', isDone: true},
+        {id: 3, title: 'CSS', isDone: true},
+        {id: 4, title: 'JS', isDone: false},
+    ])
+    const changeStatus = (id: number) => {
+        setTask(tasks.map((el) => el.id === id ? {...el, isDone: !el.isDone} : {...el}))
+    }
+    const Left = () => {
+        return <View><Text>LEFT</Text></View>
+    }
+    const Right = () => {
+        return <View><Text>RIGHT</Text></View>
+    }
+
+
+    const render: ListRenderItem<TaskType> = ({item, index}) => {
+
+        return <Swipeable renderLeftActions={Left} renderRightActions={Right}>
+            <TouchableWithoutFeedback
+                onLongPress={() => changeStatus(item.id)}>
+                <View
+                    style={{
+                        borderWidth: item.isDone ? 4 : 1,
+                        borderColor: item.isDone ? '#86b773' : 'pink',
+                        opacity: item.isDone ? 0.4 : 1,
+                        marginVertical: 5,
+                        borderRadius: 6,
+                        alignItems: "center"
+                    }}
+                >
+                    <Text style={{fontSize: 24, fontWeight: "500", paddingVertical: 10}}>{item.title}</Text>
+                </View>
+            </TouchableWithoutFeedback>
+        </Swipeable>
+    }
+
+    const addTasks = () => {
+        const newTask: TaskType = {
+            id: tasks.length + 1,
+            title: value,
+            isDone: false
+        }
+        setTask([newTask, ...tasks])
+        setValue('')
+        Keyboard.dismiss()
+    }
+
+    return (
+        <GestureHandlerRootView style={{flex: 1}}>
+
+        <View style={styles.container}>
+                <View style={{
+                    width: "100%",
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "space-between"
+                }}>
+                    <Icon1/>
+                    <TextInput style={{width: 200, height: 28, borderWidth: 1, borderRadius: 3}}
+                               value={value}
+                               onChangeText={setValue}
+                    />
+                    <TouchableOpacity onPress={() => addTasks()}>
+                        <Text>Add</Text>
+                    </TouchableOpacity>
+                </View>
+                <Text>{value}</Text>
+                <View style={{width: "100%"}}>
+                    <FlatList<TaskType>
+                        data={tasks} renderItem={render}
+                        keyExtractor={item => item.id.toString()}
+                    />
+                </View>
+                <Icon2/>
+                <Icon3/>
+                <StatusBar style='auto'/>
+            </View>
+
+            {/*<TeslaApp/>*/}
+            {/*<View style={styles.container}>*/}
 
             {/*<TouchableOpacity*/}
             {/*    style={styles.button}*/}
@@ -49,17 +138,19 @@ export default function App() {
             {/*    the visual form of a document or a typeface without relying on meaningful content.*/}
             {/*    Lorem ipsum may be used as a placeholder before final copy is available.*/}
             {/*</Text>*/}
-        {/*</View>*/}
-        </>
+            {/*</View>*/}
+        </GestureHandlerRootView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
+        marginTop: 50,
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center',
+        // justifyContent: 'center',
+        paddingHorizontal: 20,
     },
     image: {
         height: 300,
