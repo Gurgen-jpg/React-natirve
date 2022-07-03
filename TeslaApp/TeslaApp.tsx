e598cb14209def35f7ae6a1b15b07cbe0bd82cf2
Gurgen <gsarkisdganyan@gmail.com>
2022-07-03 20:24:03 +0300

    Header is done, greatApp!!!! Done first APP

--- TeslaApp/TeslaApp.tsx
+++ TeslaApp/TeslaApp.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {StyleSheet,View} from "react-native";
 import {StatusBar} from "expo-status-bar";
 import CarsList from "./components/CarsList/CarsList";
+import Header from "./components/Header/Header";
 
 
 
 export default function TeslaApp(){
     return (
         <View style={styles.container}>
+            <Header/>
             <CarsList />
             <StatusBar style="auto"/>
         </View>
